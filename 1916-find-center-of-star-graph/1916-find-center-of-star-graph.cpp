5f361b5d9818f60465c0e99ebccaf842dc4f7608
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2024-06-28 00:30:31 +0530

    Time: 124 ms (89.29%), Space: 70.7 MB (92.91%) - LeetHub

--- 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/1916-find-center-of-star-graph.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& e) {
-        vector<int> v ; 
-        int n = e.size() ; 
-        int m = e[0].size() ; 
-        for(int i =0 ; i< n ; i++){
-            for(int j = 0 ; j<m ; j++){
-                v.push_back(e[i][j]) ; 
-            }
-        }
-        sort(v.begin() , v.end()) ; 
+        // vector<int> v ; 
+        // int n = e.size() ; 
+        // int m = e[0].size() ; 
+        // for(int i =0 ; i< n ; i++){
+        //     for(int j = 0 ; j<m ; j++){
+        //         v.push_back(e[i][j]) ; 
+        //     }
+        // }
 
-        for(int i = 0 ; i<v.size()-1 ; i++){
-            if(v[i+1] == v[i]) return v[i] ;  
-        }
+        // sort(v.begin() , v.end()) ; 
 
-        return -1  ;
+        // for(int i = 0 ; i<v.size()-1 ; i++){
+        //     if(v[i+1] == v[i]) return v[i] ;  
+        // }
+        // return -1  ;
+
+        return e[0][0] == e[1][0] || e[0][0] == e[1][1] ? e[0][0] : e[0][1];
 
 
     }
