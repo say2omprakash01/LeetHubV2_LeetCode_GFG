71ae6d566cd7032d050b4bc3cc193e620613cc07
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2024-07-07 00:33:07 +0530

    Time: 2 ms (54.14%), Space: 7.1 MB (30.58%) - LeetHub

---
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int i =1 ; 
+        while(time>0){
+            while(i<n and time>0){
+                i++ ; 
+                time-- ; 
+            }
+
+            while(i> 1  and time>0){
+                i-- ; 
+                time-- ; 
+            }
+        }
+        return i ; 
+    }
+};
