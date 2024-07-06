d2e24c4f2a3fff79dead55700085075cc20930c3
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2024-07-07 00:08:44 +0530

    Time: 180 ms (18%), Space: 119.8 MB (23.42%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -10,21 +10,31 @@
  */
 class Solution {
 public:
-    vector<int> nodesBetweenCriticalPoints(ListNode* h) {
-    int first = INT_MAX, last = 0, prev_val = h->val, min_d = INT_MAX;
-    for (int i = 0; h->next != nullptr; ++i) {
-        if ((max(prev_val, h->next->val) < h->val) || 
-            (min(prev_val, h->next->val) > h->val)) {
-            if (last != 0)
-                min_d = min(min_d, i - last);
-            first = min(first, i);
-            last = i;
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        if (!head || !head->next || !head->next->next)
+            return {-1, -1};
+        ListNode* prev = head;
+        head = head->next;
+
+        vector<int> index;
+        int i = 1;
+        while (head->next) {
+            if ((prev->val < head->val && head->val > head->next->val) ||
+                (prev->val > head->val && head->val < head->next->val)) {
+                index.push_back(i);
+            }
+            prev = head;
+            head = head->next;
+            i++;
         }
-        prev_val = h->val;
-        h = h->next;
+
+        if (index.size() < 2)
+            return {-1, -1};
+        int localminima = INT_MAX;
+        for (int l = 0; l < index.size() - 1; l++) {
+            localminima = min(index[l + 1] - index[l], localminima);
+        }
+
+        return {localminima, index.back() - index[0]};
     }
-    if (min_d == INT_MAX)
-        return {-1, -1};
-    return {min_d, last - first};
-}
 };
